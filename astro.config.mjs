fc145724a50211e114e12f775c93b31f54d6382c
Tirito <99983969+Tirito6626@users.noreply.github.com>
2025-09-11 21:48:45 +0200

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide'
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: "test",
+			plugins: [starlightThemeRapide()],
+			sidebar: [
+        { 
+          label: "test",
+          autogenerate: { directory: '/' }
+			],
+		}),
+	],
+});
