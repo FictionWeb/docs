0ba8ca7e7bc0a819c748e07d0556bbbf1655fbd9
Tirito <easycraft250@gmail.com>
2025-09-28 17:20:22 +0200

    fixed links to functions ref

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
 			title: "Fiction",
 			logo: { 
 				src: './src/assets/logo.png',
-				replaceTitle: true
+				replacesTitle: true
 			},
       social: [
         { 
